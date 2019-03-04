0ae65be7e392e19806eac42c8257fe3b3c0b9f09
Briana Nzivu <bjnzivu@gmail.com>
2019-03-04 19:23:19 +0300

    st tried adding detail to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-//business logic
- var begin= {
+var begin= {
   player1Score: 0,
   player2Score: 0,
   playerUp: 1,
@@ -10,7 +9,6 @@ function dieRoll () {
   die1 = Math.floor(Math.random()*6) +1;
   return die1;
 }
-
 var playerRoll = function() {
   var roll = dieRoll();
   if(roll ===1){
@@ -29,80 +27,3 @@ var playerRoll = function() {
   }
   return roll;
 }
-
-function holdThePig() {
-  var currentPlayer = begin.playerUp;
-  if (currentPlayer ===1) {
-    begin.player1Score += begin.Score;
-  } else {
-    begin.player2Score += begin.Score;
-  }
-  begin.Score = 0;
-  switchPlayer();
-}
-
-//user interface logic
-
-function switchPlayer () {
-  if (begin.playerUp === 1) {
-    $("#player1Button").hide();
-    $("#player2Button").show();
-    begin.playerUp = 2;
-
-  } else {
-    $("#player2Button").hide();
-    $("#player1Button").show();
-    begin.playerUp = 1;
-
-  }
-}
-
-function resetGame() {
-  begin.player1Score = 0;
-  begin.player2Score = 0;
-  begin.playerUp = 1;
-  begin.Score = 0;
-}
-
-function alertEndTurn(){
-  alert("Sorry - you rolled a 1.  Your score remains the same and your turn is over.");
-  $(".playerStatus").text(begin.playerUp);
-}
-
-function alertWinner(playerNumber) {
-  alert("Player " + playerNumber + " is the BIG winner!!");
-  resetGame();
-  $(".gameStats").text(0);
-}
-
-$(document).ready(function() {
-
-  $("form#pigForm").submit(function(event){
-    var playerName1 = $("input#playerName1").val();
-    var playerName2 = $("input#playerName2").val();
-      $("span#playerName1").text(playerName1);
-      $("span#playerName2").text(playerName2);
-      $("#player2Button").hide();
-      $("#player1Button").show();
-      $(".playerStatus").text(begin.playerUp);
-      event.preventDefault();
-
-    var nameHolder = new Names(playerName1, playerName2);
-  })
-
-
-  $(".rollPig").click(function() {
-    pigResult = playerRoll();
-    $(".Result").text(pigResult);
-    $(".Score").text(begin.Score);
-
-  });
-
-  $(".holdPig").click(function(){
-    holdThePig();
-    $("Result").text("");
-    $(".player1Score").text(begin.player1Score);
-    $(".player2Score").text(begin.player2Score);
-    $(".playerStatus").text(begin.playerUp);
-  });
-});
